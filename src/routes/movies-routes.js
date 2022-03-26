14047ec6e5847213d20549f9dde39de0222fbe2b
unknown <rfcosta85@gmail.com>
2022-03-26 11:05:24 -0300

    movies routes config

---
+++ src/routes/movies-routes.js
@@ -0,0 +1,126 @@
+import * as MovieController from "../controllers/movie-controller.js";
+import { validateRequest } from "../middleware/auth.js";
+
+export default {
+  showAll: {
+    method: "GET",
+    url: "/movies",
+    handler: MovieController.index,
+  },
+  showMovie: {
+    method: "GET",
+    url: "/movies/:id",
+    schema: {
+      params: {
+        type: "object",
+        proprieties: {
+          id: { type: "number" },
+        },
+      },
+    },
+    handler: MovieController.getMovie,
+  },
+  create: {
+    method: "POST",
+    url: "/movies",
+    schema: {
+      header: {
+        type: "object",
+        properties: {
+          Authorization: { type: "string" },
+        },
+        required: ["Authorization"],
+      },
+      body: {
+        type: "object",
+        properties: {
+          title: { type: "string" },
+          description: { type: "string" },
+          gender_id: { type: "number" },
+        },
+        required: ["title", "description", "gender_id"],
+      },
+    },
+    preHandler: [validateRequest],
+    handler: MovieController.create,
+  },
+  update: {
+    method: "PUT",
+    url: "/movies/:id",
+    schema: {
+      header: {
+        type: "object",
+        properties: {
+          Authorization: { type: "string" },
+        },
+        required: ["Authorization"],
+      },
+      params: {
+        type: "object",
+        properties: {
+          id: { type: "number" },
+        },
+      },
+      body: {
+        type: "object",
+        properties: {
+          title: { type: "string" },
+          description: { type: "string" },
+          gender_id: { type: "number" },
+        },
+      },
+    },
+    preHandler: [validateRequest],
+    handler: MovieController.update,
+  },
+  updateAtributes: {
+    method: "PATCH",
+    url: "/movies/:id",
+    schema: {
+      header: {
+        type: "object",
+        properties: {
+          Authorization: { type: "string" },
+        },
+        required: ["Authorization"],
+      },
+      params: {
+        type: "object",
+        properties: {
+          id: { type: "number" },
+        },
+      },
+      body: {
+        type: "object",
+        properties: {
+          title: { type: ["string", "null"] },
+          description: { type: ["string", "null"] },
+          gender_id: { type: ["number", "null"] },
+        },
+      },
+    },
+    preHandler: [validateRequest],
+    handler: MovieController.updateAtributes,
+  },
+  delete: {
+    method: "DELETE",
+    url: "/movies/:id",
+    schema: {
+      header: {
+        type: "object",
+        properties: {
+          Authorization: { type: "string" },
+        },
+        required: ["Authorization"],
+      },
+      params: {
+        type: "object",
+        properties: {
+          id: { type: "number" },
+        },
+      },
+    },
+    preHandler: [validateRequest],
+    handler: MovieController.remove,
+  },
+};
